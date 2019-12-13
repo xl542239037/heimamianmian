1c477f956f32b8c4806673910841d417d1d0c3d1
笨笨的机器人 <18710143839@163.COM>
2019-12-13 16:53:17 +0800

    整合elementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './routers/router'
 Vue.config.productionTip = false
+//element ui插件导入
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//注册到vue
+Vue.use(ElementUI);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/routers/router.js
@@ -0,0 +1,17 @@
+// 导入 vue-router
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+//注册 vue-router
+Vue.use(VueRouter)
+// 实例化
+const router = new VueRouter({
+//这里就是路由的配制项
+routes: [
+//  {
+// path: '/这里可给路由地址一个名字',
+// component:  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+//  }
+  ]
+})
+export default router
